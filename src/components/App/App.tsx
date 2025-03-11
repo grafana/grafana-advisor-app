61d03170add722b3a1f68ffa36901a419adf8a80
Andres Martinez Gotor <andres.martinez@grafana.com>
2025-03-11 15:58:48 +0100

    review

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,14 +6,13 @@ import FeatureFlagMissing from 'pages/FeatureFlagMissing';
 const Home = React.lazy(() => import('../../pages/Home'));
 
 export default function App(props: AppRootProps) {
-  let component = <Home />;
   // TODO: Remove this once the feature flag is enabled by default
   if (!(config.featureToggles as any)['grafanaAdvisor']) {
-    component = <FeatureFlagMissing />;
+    return <FeatureFlagMissing />;
   }
   return (
     <Routes>
-      <Route path="*" element={component} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }
