f9e5dc14cb57378354e435b286c9e95dd976252b
Andres Martinez Gotor <andres.martinez@grafana.com>
2025-03-11 16:03:42 +0100

    Add page when missing the feature flag (#21)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AppRootProps } from '@grafana/data';
+import { config } from '@grafana/runtime';
+import FeatureFlagMissing from 'pages/FeatureFlagMissing';
 const Home = React.lazy(() => import('../../pages/Home'));
 
 export default function App(props: AppRootProps) {
+  // TODO: Remove this once the feature flag is enabled by default
+  if (!(config.featureToggles as any)['grafanaAdvisor']) {
+    return <FeatureFlagMissing />;
+  }
   return (
     <Routes>
       <Route path="*" element={<Home />} />
