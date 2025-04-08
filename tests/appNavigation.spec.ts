0c27e36b4a59ad74e235d397acdb4f879bb82e31
Andres Martinez Gotor <andres.martinez@grafana.com>
2025-04-08 12:53:55 +0200

    Enable e2e test

--- tests/appNavigation.spec.ts
+++ tests/appNavigation.spec.ts
@@ -1,31 +1,22 @@
 import { test, expect } from './fixtures';
-import { ROUTES } from '../src/constants';
 
 test.describe('navigating app', () => {
-  test('page one should render successfully', async ({ gotoPage, page }) => {
-    await gotoPage(`/${ROUTES.One}`);
-    await expect(page.getByText('This is page one.')).toBeVisible();
-  });
-
-  test('page two should render successfully', async ({ gotoPage, page }) => {
-    await gotoPage(`/${ROUTES.Two}`);
-    await expect(page.getByText('This is page two.')).toBeVisible();
-  });
-
-  test('page three should support an id parameter', async ({ gotoPage, page }) => {
-    await gotoPage(`/${ROUTES.Three}/123456`);
-    await expect(page.getByText('ID: 123456')).toBeVisible();
-  });
-
-  test('page three should render sucessfully', async ({ gotoPage, page }) => {
-    // wait for page to successfully render
-    await gotoPage(`/${ROUTES.One}`);
-    await expect(page.getByText('This is page one.')).toBeVisible();
-
-    // navigating to page four with full width layout without sidebar menu
-    await page.getByText('Full-width page example').click();
-
-    // navigate back to page one
-    await page.getByRole('link', { name: 'Back', exact: true }).click();
+  test('home page should render successfully', async ({ gotoPage, page }) => {
+    await gotoPage(`/`);
+    await expect(page.getByText('Keep Grafana running smoothly and securely')).toBeVisible();
+    // It should delete the report
+    await page.getByRole('button', { name: 'Delete reports' }).click();
+    await page.getByRole('button', { name: 'Confirm' }).click();
+    // Page should be empty
+    await expect(page.getByText('No report found')).toBeVisible();
+    // Click on the "Refresh" button
+    await page.getByRole('button', { name: 'Refresh' }).click();
+    // Page should now show a report
+    await expect(page.getByText('More info')).toBeVisible();
+    // Click on the "More info"
+    await page.getByText('More info').click();
+    // Page should now show a report
+    await expect(page.getByText('datasource(s) analyzed')).toBeVisible();
+    await expect(page.getByText('plugin(s) analyzed')).toBeVisible();
   });
 });
