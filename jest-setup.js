b211c33063994705afb3c80c28ee047497fe6af7
Andres Martinez Gotor <andres.martinez@grafana.com>
2025-07-18 13:21:38 +0200

    fix test

--- jest-setup.js
+++ jest-setup.js
@@ -1,6 +1,12 @@
 // Jest setup provided by Grafana scaffolding
 import './.config/jest-setup';
 
+// Mock @grafana/runtime to provide usePluginInteractionReporter
+jest.mock('@grafana/runtime', () => ({
+  ...jest.requireActual('@grafana/runtime'),
+  usePluginInteractionReporter: jest.fn(() => jest.fn()),
+}));
+
 // Global mocks to avoid Redux context issues in tests
 jest.mock('api/api', () => ({
   ...jest.requireActual('api/api'),
